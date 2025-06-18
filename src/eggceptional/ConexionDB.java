9d9704cfe6d4c59638ac08e20e9fe467c906a7fd
jjtl147 <leyvajhamil@gmail.com>
2025-06-18 13:25:45 -0400

    es lo que se va a ver en el commit

--- src/eggceptional/ConexionDB.java
+++ src/eggceptional/ConexionDB.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class ConexionDB {
     private static final String URL = "jdbc:postgresql://localhost:5432/eggceptional";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "bauty//3210";
+    private static final String PASSWORD = "2005";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+//mamaguevo
